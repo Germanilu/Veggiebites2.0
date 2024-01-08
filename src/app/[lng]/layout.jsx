ba552fc85bba2e96fe6587660ed7358a11ed00d8
Germanilu <lucianogermani93@gmail.com>
2024-01-09 00:10:02 +0100

    Feat: Adding i18n to project

--- src/app/[lng]/layout.jsx
+++ src/app/[lng]/layout.jsx
@@ -4,14 +4,27 @@ import Navbar from './components/navbar/index';
 import Footer from './components/footer/index';
 const inter = Inter({ subsets: ['latin'] })
 
+import { dir } from 'i18next'
+
+import { languages } from '../i18n/settings';
+
 export const metadata = {
   title: 'Flavour Frame',
   description: 'Crafting Flavorful Frames',
 }
 
-export default function RootLayout({ children }) {
+export async function generateStaticParams() {
+  return languages.map((lng) => ({ lng }))
+}
+
+export default function RootLayout({ 
+  children,
+  params: {
+    lng
+  },
+ }) {
   return (
-    <html lang="en">
+    <html lang={lng} dir={dir(lng)}>
       <body className={inter.className}>
         <Navbar/>
         {children}
--- src/app/[lng]/page.jsx
+++ src/app/[lng]/page.jsx
@@ -1,11 +1,16 @@
 import './page.scss'
-export default function Home() {
-
+import Link from 'next/link'
+export default function Home({ params: { lng } }) {
+  // https://locize.com/blog/next-app-dir-i18n/
   return (
     <div className='home-design'>
       <div className="hero-section">
         <h1 className='hero-title'>TITULO</h1>
       </div>
+      <h1>Hi there!</h1>
+      <Link href={`/${lng}/about`}>
+        second page
+      </Link>
       <div className="first-section">
         <h2>FIRST SECTION</h2>
       </div>
