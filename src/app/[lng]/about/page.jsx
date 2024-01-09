aaf3ab9ce5b9cadd290d95dfb4d4249b01e6553b
Germanilu <lucianogermani93@gmail.com>
2024-01-09 23:54:06 +0100

    Feat: i18n translate implementation

--- src/app/[lng]/about/page.jsx
+++ src/app/[lng]/about/page.jsx
@@ -1,8 +1,12 @@
 import './page.scss';
-export default function About(){
+import { useTranslation } from '../../i18n';
+export default async function About({ params: { lng } }){
+
+  const { t } = await useTranslation(lng, 'about')
+
   return (
     <div className="about-design">
-      <h1>About</h1>
+      <h1>{t('title')}</h1>
     </div>
   ) 
 }
--- src/app/[lng]/page.jsx
+++ src/app/[lng]/page.jsx
@@ -1,11 +1,16 @@
 import './page.scss'
 import Link from 'next/link'
-export default function Home({ params: { lng } }) {
+import { useTranslation } from '../i18n'
+
+export default async function Home({ params: { lng } }) {
   // https://locize.com/blog/next-app-dir-i18n/
+  
+  const { t } = await useTranslation(lng);
+  console.log("HOLA",t)
   return (
     <div className='home-design'>
       <div className="hero-section">
-        <h1 className='hero-title'>TITULO</h1>
+        <h1 className='hero-title'>{t('title')}</h1>
       </div>
       <h1>Hi there!</h1>
       <Link href={`/${lng}/about`}>
