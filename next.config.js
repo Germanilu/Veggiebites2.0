5954f77406c15d36ec0b9da99290aa42aa0e123e
Luciano Germani <lucianogermani93@gmail.com>
2024-01-14 00:42:19 +0100

    fix i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = { 
-}
+const nextConfig = {}
 
-module.exports = nextConfig
+const withNextIntl = require('next-intl/plugin')(
+    './i18n.js'
+);
+
+module.exports = withNextIntl(nextConfig)
+
---
+++ src/app/[locale]/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import './index.scss'
+export default function Footer(){
+  return (
+    <div className="footer-design">
+      <h1>FOOTER</h1>
+    </div>
+  ) 
+}
+
+
+
