c12c21d8039ceff15b70e62efbd08e628ca63876
Germanilu <lucianogermani93@gmail.com>
2024-08-16 09:56:49 +0200

    Feat: add hero

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,6 +1,7 @@
 import { useTranslations } from "next-intl";
 import Map from "@/app/[locale]/components/google-map-location/index";
 import ScrollToTop from "@/app/[locale]/components/scroll-to-top/index";
+import Hero from "./hero";
 import './page.scss';
 
 export const metadata = {
@@ -13,17 +14,7 @@ export default function Home() {
 
   return (
     <div className='home-design'>
-      <div className="hero-section">
-        <h1 className='hero-title'>{t('title')}
-          
-        </h1>
-      </div>
-      <div className="first-section">
-        <h2>{t('first-section-title')}</h2>
-      </div>
-      <div className="second-section">
-        <h2>{t('second-section-title')}</h2>
-      </div>
+      <Hero/>
       <Map />
       <ScrollToTop />
     </div>
